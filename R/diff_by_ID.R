1957f6ac4f1f1bae2c805085b1bc62a4dbfa43a1
Théo Michelot <tm75@st-andrews.ac.uk>
2021-03-01 17:58:45 +0000

    diff_by_ID utility function

---
+++ R/diff_by_ID.R
@@ -0,0 +1,27 @@
+
+#' First-order difference for grouped data
+#'
+#' @param x Vector of values
+#' @param ID Vector of group IDs, of same length as x
+#'
+#' @return Vector of same length as x of first-order differences of x
+#' (and NAs for last value of each group ID)
+#'
+#' @export
+diff_by_ID <- function(x, ID) {
+    if(length(x) != length(ID)) {
+        stop("'x' and 'ID' must be vectors of the same length")
+    }
+    n <- length(ID)
+
+    # Indices of first and last value of each group
+    i0 <- which(ID[-1] != ID[-n])
+    i_first <- c(1, i0 + 1)
+    i_last <- c(i0, n)
+
+    # First-order differences
+    dx <- rep(NA, n)
+    dx[-i_last] <- x[-i_first] - x[-i_last]
+
+    return(dx)
+}
