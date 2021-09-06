905ac2bb385d2adf7571ffc7278e95328f80d69b
Théo Michelot <tm75@st-andrews.ac.uk>
2021-09-06 14:12:01 +0100

    Add colour palettes

---
+++ R/colours.R
@@ -0,0 +1,9 @@
+
+#' hmmTMB colour palette
+pal1 <- c("#00798c", "#d1495b", "#edae49", "#66a182", "#2e4057", "#8d96a3")
+
+#' moveHMM colour palette (Okabe and Ito)
+pal2 <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+#' Color Brewer palette
+pal3 <- c('#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02','#a6761d','#666666')
